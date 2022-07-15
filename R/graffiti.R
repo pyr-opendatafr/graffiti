813e90d95988b5028830f21fed3698afd26134cf
jdesclodure <103088250+jdesclodure@users.noreply.github.com>
2022-07-15 10:21:31 +0200

    giving the new app folder to system.file

--- R/graffiti.R
+++ R/graffiti.R
@@ -8,7 +8,7 @@
 #' }
 #' @export
 graffiti <- function() {
-  appDir <- system.file("shiny", "frenchEconomy", package = "graffiti")
+  appDir <- system.file("shiny", "graffiti-app", package = "graffiti")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `graffiti`.", call. = FALSE)
   }
